66c84e55ce6279738617bf79f265a628aabf7b98
tugceozcakir <tugceozcakir15@gmail.com>
2023-08-08 20:36:33 +0300

    update

--- example/src/main/java/com/tugceozcakir/example/util/BaseController.java
+++ example/src/main/java/com/tugceozcakir/example/util/BaseController.java
@@ -2,6 +2,7 @@ package com.tugceozcakir.example.util;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
