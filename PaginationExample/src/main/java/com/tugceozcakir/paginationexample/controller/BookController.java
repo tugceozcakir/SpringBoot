3e8ce13b48c17434bda928d570c422fb22a12a06
tugceozcakir <tugceozcakir15@gmail.com>
2023-08-16 20:38:03 +0300

    update

--- PaginationExample/src/main/java/com/tugceozcakir/paginationexample/controller/BookController.java
+++ PaginationExample/src/main/java/com/tugceozcakir/paginationexample/controller/BookController.java
@@ -1,16 +1,11 @@
-package com.tugceozcakir.paginationexample.api;
+package com.tugceozcakir.paginationexample.controller;
 
-import com.tugceozcakir.paginationexample.database.entity.BookEntity;
-import com.tugceozcakir.paginationexample.database.repository.BookRepository;
 import com.tugceozcakir.paginationexample.model.BookDTO;
 import com.tugceozcakir.paginationexample.model.PageDTO;
 import com.tugceozcakir.paginationexample.model.requestDTO.BookRequestDTO;
 import com.tugceozcakir.paginationexample.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
